5275b5529352933cf1bc3185d4e5da1870fa1e59
Pavel <pavel.agejkin@gmail.com>
2021-02-26 10:02:00 +0000

    fix fscore

--- src/main/java/es/uam/ir/ranksys/metrics/basic/FScore.java
+++ src/main/java/es/uam/ir/ranksys/metrics/basic/FScore.java
@@ -10,7 +10,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package es.uam.ir.ranksys.metrics.basic;;
+package es.uam.ir.ranksys.metrics.basic;
 
         import es.uam.eps.ir.ranksys.metrics.basic.Precision;
         import es.uam.eps.ir.ranksys.metrics.basic.Recall;
